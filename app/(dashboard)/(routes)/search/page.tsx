17ff7f73f8f45abc573592d0696f61fba2280160
Sayak <sayakmodak2003@gmail.com>
2024-09-12 16:13:15 +0530

    added Search Query

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,6 +1,7 @@
 import { db } from '@/lib/db';
 import React from 'react';
 import { Categories } from './_components/categories';
+import { SearchInput } from '@/components/search-input';
 
 const Searchpage = async () => {
   const categories = await db.category.findMany({
@@ -15,9 +16,14 @@ const Searchpage = async () => {
   //   });
 
   return (
-    <div className="p-6">
-      <Categories items={categories} />
-    </div>
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
+    </>
   );
 };
 
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 import { LogOut } from 'lucide-react';
 import Link from 'next/link';
+import { SearchInput } from './search-input';
 
 const NavbarRoutes = () => {
   const pathname = usePathname();
@@ -12,27 +13,36 @@ const NavbarRoutes = () => {
 
   const isTeacherPage = pathname?.startsWith('/teacher');
   const isPlayerPage = pathname?.startsWith('/chapter');
+  const isSearchPage = pathname === '/search';
 
   return (
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacherPage || isPlayerPage ? (
-        <Link href="/">
-          <Button size="sm" variant="ghost">
-            <LogOut className="h-4 w-4 mr:2" />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button size="sm" variant="ghost">
-            Teacher Mode
-          </Button>
-        </Link>
+    <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
       )}
-      <div className="flex items-center">
-        <UserButton />
+
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacherPage || isPlayerPage ? (
+          <Link href="/">
+            <Button size="sm" variant="ghost">
+              <LogOut className="h-4 w-4 mr:2" />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button size="sm" variant="ghost">
+              Teacher Mode
+            </Button>
+          </Link>
+        )}
+        <div className="flex items-center">
+          <UserButton />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
